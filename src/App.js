6f03a3a11f075fb8070662f46a9e4301b1c6db39
ao donovan <a.o.donovan.music@gmail.com>
2024-12-13 16:02:55 -0500

    react assignment complete for real this time

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Hello from './Hello';
 import { useState, useEffect } from 'react';
+import Post from './Post';
 
 function App() {
 
   const [name, setName] = useState("");
-  const [comments, setComments] = useState(null);
   const [posts, setPosts] = useState(null);
 
   function handleChange(e){
@@ -14,55 +14,103 @@ function App() {
   }
 
   useEffect(() => {
-    const comments = [
-      {
-        comment: "looks good"
-      }, 
-      {
-        comment: "i hate it"
-      },
-      {
-        comment: "straight to jail"
-      }, 
-      {
-        comment: "beep boop"
-      }
-    ]
-
     const posts = [
       {
-        post: "post1"
+        post: "post1",
+        comments: [
+          {
+            comment: "looks good"
+          }, 
+          {
+            comment: "i hate it"
+          },
+          {
+            comment: "straight to jail"
+          }, 
+          {
+            comment: "beep boop"
+          }
+        ]
       },
       {
-        post: "post2"
+        post: "post2",
+        comments: [
+          {
+            comment: "ererwer"
+          }, 
+          {
+            comment: "asdfasdf"
+          },
+          {
+            comment: "swerwer"
+          }, 
+          {
+            comment: "beep boop"
+          }
+        ]
       },
       {
-        post: "post3"
+        post: "post3",
+        comments: [
+          {
+            comment: "l343245od"
+          }, 
+          {
+            comment: "i ha13245123it"
+          },
+          {
+            comment: "s234236 to jail"
+          }, 
+          {
+            comment: "be4545op"
+          }
+        ]
       },
       {
-        post: "post4"
+        post: "post4",
+        comments: [
+          {
+            comment: "lo23423"
+          }, 
+          {
+            comment: "asdfasdfasd it"
+          },
+          {
+            comment: "straight sdfghsdfhjail"
+          }, 
+          {
+            comment: "beep boop"
+          }
+        ]
       }
     ]
-
-
-    setComments(comments);
     setPosts(posts);
   },[])
 
-  
+  console.log(posts)
+
+  const mylist = [
+    "a",
+    "b",
+    "c"
+  ]
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {mylist.map(l => <li>{l}</li>)}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <Hello secret={"SECRET"}/>
         <h1>{name}</h1>
         <input type='text' onChange={handleChange}/>
-        {posts?.map(p => <li>{p.post}</li>)}
-        {comments?.map(c => <li>{c.comment}</li>)}
+
+
+        {posts?.map(p => <Post post={p}/>)}
+
+
+
         <a
           className="App-link"
           href="https://reactjs.org"
